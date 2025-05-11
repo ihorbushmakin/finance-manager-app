760f0c0b575ca33e74a51039c0bee3ee0d1a4b50
Ihor Bushmakin <ihorbushmakin@gmail.com>
2025-05-11 09:57:57 +0200

    add DJANGO_SETTINGS_MODULE in manage.py

--- manage.py
+++ manage.py
@@ -4,6 +4,9 @@ import os
 import sys
 
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
