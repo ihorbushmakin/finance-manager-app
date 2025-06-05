64c7f02d56840415c55a7f28c327348f0d1c7bdf
Ihor Bushmakin <ihorbushmakin@gmail.com>
2025-06-05 19:07:31 +0200

    added includes axios for api variable

--- react-frontend/src/pages/Register.js
+++ react-frontend/src/pages/Register.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import api from "../api/axios";
 
 export default function Register() {
   const [formData, setFormData] = useState({ username: "", password: "", repeatPassword: "" });
